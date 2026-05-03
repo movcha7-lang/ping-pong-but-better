2ad28a8738af8f0083fd9fbcb32b3d6f1d63b4bf
Bobik:D <movcha7@gmail.com>
2026-05-03 10:53:02 -0400

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+from pygame import*
+
+init()
+
+mixer.init()
+sound_wallhit = mixer.Sound('beep.wav')
+sound_wallhit.set_volume(1.0)
+
+sound_bounce = mixer.Sound('bounce.wav')
+sound_bounce.set_volume(1.0)
+
+sound_loss = mixer.Sound('loss.wav')
+sound_loss.set_volume(1.0)
+
+sound_victory = mixer.Sound('victory.wav')
+sound_victory.set_volume(1.0)
+
+sound_wallhit.play()
